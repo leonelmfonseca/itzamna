baed65109afb95dc1f985243fb1f5b84e04b2f39
leonelmfonseca <leonelfonseca@gmail.com>
2025-02-22 17:06:32 +0000

    feat: initial commit

---
+++ src/main/java/com/leonel/itzamna/ItzamnaApplication.java
@@ -0,0 +1,13 @@
+package com.leonel.itzamna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItzamnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItzamnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonel/itzamna/ItzamnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonel.itzamna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItzamnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
