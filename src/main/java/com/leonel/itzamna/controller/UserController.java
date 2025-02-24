5af7d676a6ee6d802857d313c46d280230185023
leonelmfonseca <leonelfonseca@gmail.com>
2025-02-24 10:32:15 +0000

    feat(api): set api versioning

--- src/main/java/com/leonel/itzamna/controller/UserController.java
+++ src/main/java/com/leonel/itzamna/controller/UserController.java
@@ -2,13 +2,11 @@ package com.leonel.itzamna.controller;
 
 import com.leonel.itzamna.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-// QUESTION: @Annotation RequestMapping here
-@RequestMapping
+@RequestMapping("/api/v1")
 public class UserController {
     
     UserService userService;
